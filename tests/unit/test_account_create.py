ee4984bbeda81e3dcaeac96bbad521c54c873bb7
Paweł Bagłaj <ProRedQ@users.noreply.github.com>
2025-10-16 19:36:07 +0200

    Feature 1-5

--- tests/unit/test_account_create.py
+++ tests/unit/test_account_create.py
@@ -1,6 +1,5 @@
 from src.account import Account
 
-
 class TestAccount:
     def test_account_creation(self):
         account = Account("John", "Doe", "12345678901")
@@ -10,19 +9,19 @@ class TestAccount:
         assert account.pesel == "12345678901"
         
     def test_pesel_too_long(self):
-        account = Account("John", "Doe", "12345678901")
-        assert account.pesel != 'Invalid'
+        account = Account("John", "Doe", "123456789011")
+        assert account.pesel == 'Invalid'
 
     def test_pesel_too_short(self):
-        account = Account("John", "Doe", "12345678901")
-        assert account.pesel != 'Invalid'
+        account = Account("John", "Doe", "1234567890")
+        assert account.pesel == 'Invalid'
 
     def test_pesel_none(self):
-        account = Account("John", "Doe", "12345678901")
-        assert account.pesel != 'Invalid'
+        account = Account("John", "Doe", "")
+        assert account.pesel == 'Invalid'
 
     def test_correct_promo_code(self):
-        account = Account("John", "Doe", "12345678901", "PROM_123")
+        account = Account("John", "Doe", "00302012345", "PROM_123")
         assert account.balance == 50
 
     def test_promo_code_suffix_too_long(self):
@@ -33,8 +32,18 @@ class TestAccount:
         account = Account("John", "Doe", "12345678901", "PROM_XY")
         assert account.balance == 0.0
 
-    def test_wrong_prefix(self):
+    def test_wrong_promo_code_prefix(self):
         account = Account("John", "Doe", "12345678901", "PRO_XYZ")
         assert account.balance == 0.0
+
+    def test_pesel_after_1960(self):
+        account = Account("John", "Doe", "00302012345")
+        year_of_birth = Account.birth_year_from_pesel(account.pesel)
+        assert year_of_birth > 1960
+
+    def test_pesel_before_1960(self):
+        account = Account("John", "Doe", "44051401458")
+        year_of_birth = Account.birth_year_from_pesel(account.pesel)
+        assert year_of_birth < 1960
         
 
