72dbb3c5de0b8b1de6919bc297d211877547fed4
Pawel Baglaj <184997716+ProRedQ@users.noreply.github.com>
2025-10-16 15:54:08 +0200

    feature 4

--- tests/unit/test_account_create.py
+++ tests/unit/test_account_create.py
@@ -3,8 +3,38 @@ from src.account import Account
 
 class TestAccount:
     def test_account_creation(self):
-        account = Account("John", "Doe", '04040414041')
+        account = Account("John", "Doe", "12345678901")
         assert account.first_name == "John"
         assert account.last_name == "Doe"
         assert account.balance == 0.0
-        assert len(account.pesel) == 11
+        assert account.pesel == "12345678901"
+        
+    def test_pesel_too_long(self):
+        account = Account("John", "Doe", "12345678901")
+        assert account.pesel != 'Invalid'
+
+    def test_pesel_too_short(self):
+        account = Account("John", "Doe", "12345678901")
+        assert account.pesel != 'Invalid'
+
+    def test_pesel_none(self):
+        account = Account("John", "Doe", "12345678901")
+        assert account.pesel != 'Invalid'
+
+    def test_correct_promo_code(self):
+        account = Account("John", "Doe", "12345678901", "PROM_123")
+        assert account.balance == 50
+
+    def test_promo_code_suffix_too_long(self):
+        account = Account("John", "Doe", "12345678901", "PROM_XYZZ")
+        assert account.balance == 0.0
+
+    def test_promo_code_suffix_too_short(self):
+        account = Account("John", "Doe", "12345678901", "PROM_XY")
+        assert account.balance == 0.0
+
+    def test_wrong_prefix(self):
+        account = Account("John", "Doe", "12345678901", "PRO_XYZ")
+        assert account.balance == 0.0
+        
+
