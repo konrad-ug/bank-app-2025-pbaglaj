6b7f843fab006d3ffe7a0022b4e39a07c1a12e4b
Pawel Baglaj <184997716+ProRedQ@users.noreply.github.com>
2025-10-16 15:12:31 +0200

    feature 1,2,3

--- tests/unit/test_account_create.py
+++ tests/unit/test_account_create.py
@@ -3,6 +3,8 @@ from src.account import Account
 
 class TestAccount:
     def test_account_creation(self):
-        account = Account("John", "Doe")
+        account = Account("John", "Doe", '04040414041')
         assert account.first_name == "John"
         assert account.last_name == "Doe"
+        assert account.balance == 0.0
+        assert len(account.pesel) == 11
