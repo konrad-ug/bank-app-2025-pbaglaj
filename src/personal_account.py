381883afcd2291b1ad02fc581c9d3480b9b6db52
Paweł Bagłaj <ProRedQ@users.noreply.github.com>
2025-11-20 19:12:16 +0100

    Feature 14

--- src/personal_account.py
+++ src/personal_account.py
@@ -72,3 +72,21 @@ class PersonalAccount(Account):
         """Sprawdza, czy suma 5 ostatnich transakcji przekracza żądaną kwotę pożyczki."""
         return len(self.history) >= 5 and sum(self.history[-5:]) > amount
 
+class AccountRegistry:
+    def __init__(self):
+        self.accounts = []
+
+    def add_account(self, account: PersonalAccount):
+        self.accounts.append(account)
+
+    def find_account_by_pesel(self, pesel: str) -> PersonalAccount | None:
+        for account in self.accounts:
+            if account.pesel == pesel:
+                return account
+        return None
+    
+    def return_all_accounts(self) -> list[PersonalAccount]:
+        return self.accounts
+    
+    def return_amount_of_accounts(self) -> int:
+        return len(self.accounts)
